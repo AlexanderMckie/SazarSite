164b36c016923d14598da3abe03537c7059964d7
AlexanderMckie <alex@sitara.net.au>
2025-03-07 21:36:54 +0800

    test

--- assets/js/headerAnimation.js
+++ assets/js/headerAnimation.js
@@ -25,74 +25,74 @@ const cloudBackArray = [
 
 const sazPieceFilePath = "assets/images/SazPiece.svg";
 const sazPieceElementID = "SazPiece";
-function adjustSVGSize() {
-    const svgContainer = document.getElementById(sazPieceElementID);
-    if (window.innerWidth < 768) { // Mobile view
-       
-        svgContainer.style.height = '60vh';
-        svgContainer.style.overflowX = 'auto';
-        svgContainer.style.overflowY = 'hidden';
-        svgContainer.style.whiteSpace = 'nowrap';
-    } else { // Desktop view
-        svgContainer.style.width = '100%';
-        svgContainer.style.height = 'auto';
-        svgContainer.style.overflow = 'hidden';
-    }
-}
-let scrollAnimationFrame;
-
-// function autoScrollSVG() {
+// function adjustSVGSize() {
 //     const svgContainer = document.getElementById(sazPieceElementID);
-//     const svgObject = svgContainer.querySelector('object.svg-object');
-//     if (window.innerWidth < 768 && svgObject) { // Mobile view
-//         const maxScroll = svgObject.clientWidth - svgContainer.clientWidth;
-//         if (scrollAnimationFrame) {
-//             cancelAnimationFrame(scrollAnimationFrame);
-//         }
-//         gsap.to(svgObject, {
-//             x: -maxScroll,
-//             duration: 10, // Adjust duration to control the scroll speed
-//             ease: "none",
-//             repeat: -1, // Infinite loop
-//             startAt: { x: -maxScroll * -0.5, opacity: 0 }, // Start from the left position with 0 opacity
-//             endAt: { x: 0, opacity: 0 },
-//             modifiers: {
-//             x: gsap.utils.unitize(x => parseFloat(x) % maxScroll) // Loop the scroll
-//             },
-//             onStart: () => {
-//                 svgContainer.style.overflowX = 'hidden'; // Hide the scrollbar
-//             },
-//             onComplete: () => {
-//                 svgContainer.style.overflowX = 'auto'; // Show the scrollbar after animation
-//             },
-//             onUpdate: function() {
-//             const progress = gsap.getProperty(svgObject, "x") / maxScroll;
-//             if (progress < 0.1) {
-//                 gsap.to(svgObject, { opacity: progress * 10, duration: 1 });
-//             } else if (progress > 0.9) {
-//                 gsap.to(svgObject, { opacity: (1 - progress) * 10, duration: 1 });
-//             } else {
-//                 gsap.to(svgObject, { opacity: 1, duration: 0 });
-//             }
-//             }
-//         });
+//     if (window.innerWidth < 768) { // Mobile view
+       
+//         svgContainer.style.height = '60vh';
+//         svgContainer.style.overflowX = 'auto';
+//         svgContainer.style.overflowY = 'hidden';
+//         svgContainer.style.whiteSpace = 'nowrap';
+//     } else { // Desktop view
+//         svgContainer.style.width = '100%';
+//         svgContainer.style.height = 'auto';
+//         svgContainer.style.overflow = 'hidden';
 //     }
 // }
+// let scrollAnimationFrame;
+
+// // function autoScrollSVG() {
+// //     const svgContainer = document.getElementById(sazPieceElementID);
+// //     const svgObject = svgContainer.querySelector('object.svg-object');
+// //     if (window.innerWidth < 768 && svgObject) { // Mobile view
+// //         const maxScroll = svgObject.clientWidth - svgContainer.clientWidth;
+// //         if (scrollAnimationFrame) {
+// //             cancelAnimationFrame(scrollAnimationFrame);
+// //         }
+// //         gsap.to(svgObject, {
+// //             x: -maxScroll,
+// //             duration: 10, // Adjust duration to control the scroll speed
+// //             ease: "none",
+// //             repeat: -1, // Infinite loop
+// //             startAt: { x: -maxScroll * -0.5, opacity: 0 }, // Start from the left position with 0 opacity
+// //             endAt: { x: 0, opacity: 0 },
+// //             modifiers: {
+// //             x: gsap.utils.unitize(x => parseFloat(x) % maxScroll) // Loop the scroll
+// //             },
+// //             onStart: () => {
+// //                 svgContainer.style.overflowX = 'hidden'; // Hide the scrollbar
+// //             },
+// //             onComplete: () => {
+// //                 svgContainer.style.overflowX = 'auto'; // Show the scrollbar after animation
+// //             },
+// //             onUpdate: function() {
+// //             const progress = gsap.getProperty(svgObject, "x") / maxScroll;
+// //             if (progress < 0.1) {
+// //                 gsap.to(svgObject, { opacity: progress * 10, duration: 1 });
+// //             } else if (progress > 0.9) {
+// //                 gsap.to(svgObject, { opacity: (1 - progress) * 10, duration: 1 });
+// //             } else {
+// //                 gsap.to(svgObject, { opacity: 1, duration: 0 });
+// //             }
+// //             }
+// //         });
+// //     }
+// // }
 
 
 
-window.addEventListener('resize', () => {
+// window.addEventListener('resize', () => {
 
-    //autoScrollSVG();
-    //adjustSVGSize();
+//     //autoScrollSVG();
+//     //adjustSVGSize();
     
-});
+// });
 
-if (window.innerWidth < 768) {
+// if (window.innerWidth < 768) {
 
-    //autoScrollSVG();
+//     //autoScrollSVG();
 
-}
+// }
 
 
 function loadSVGIntoElement(svgPath, containerElementId) {
@@ -147,74 +147,74 @@ document.addEventListener('DOMContentLoaded', () => {
         .catch(error => {
             console.error('Error loading SVG into element:', error);
         });
-
-function initializeSVGElements(keylineGroup, starGroup) {
-    if (keylineGroup) {
-        keylineGroup.style.opacity = 0; // Set initial opacity to 0
-    }
-    if (starGroup) {
-        starGroup.forEach(star => {
-            if (star) star.style.opacity = 0; // Set initial opacity to 0 for each star, making them invisible
-        });
-    }
-}
-
-function addEventListenersToPieceGroup(pieceGroup, keylineGroup, starGroup) {
-    if (pieceGroup) {
-        pieceGroup.addEventListener('mouseover', () => {
-            //animateKeylineAndStars(keylineGroup, starGroup);
-        });
-
-        pieceGroup.addEventListener('touchstart', () => {
-            //animateKeylineAndStars(keylineGroup, starGroup);
-        });
-
-        pieceGroup.addEventListener('mouseout', () => {
-            gsap.to(keylineGroup, {
-                opacity: 0,
-                duration: 1,
-                ease: "power1.in"
-            });
-        });
-    } else {
-        console.error("SVG group with ID 'Piece' not found.");
-    }
-}
-
-function animateKeylineAndStars(keylineGroup, starGroup) {
-    gsap.to(keylineGroup, {
-        opacity: 1,
-        duration: 0.3,
-        ease: "power1.out"
     });
-    starGroup.forEach((star, index) => {
-        for (let i = 0; i < 1; i++) {
-            const starCopy = star.cloneNode(true);
-            star.parentNode.insertBefore(starCopy, star.nextSibling);
+// function initializeSVGElements(keylineGroup, starGroup) {
+//     if (keylineGroup) {
+//         keylineGroup.style.opacity = 0; // Set initial opacity to 0
+//     }
+//     if (starGroup) {
+//         starGroup.forEach(star => {
+//             if (star) star.style.opacity = 0; // Set initial opacity to 0 for each star, making them invisible
+//         });
+//     }
+// }
 
-            gsap.fromTo(starCopy, 
-                { opacity: 1 },
-                {
-                    motionPath: {
-                        path: [
-                            {x: 0, y: 0},
-                            {x: 3, y: -3},
-                            {x: -2, y: -6},
-                            {x: 2, y: -9},
-                            {x: -2, y: -12}
-                        ],
-                        curviness: .1,
-                        autoRotate: false
-                    },
-                    opacity: 0,
-                    duration: 1 + Math.random(),
-                    delay: index * 0.1 + i * 0.2,
-                    ease: "power1.out",
-                    onStart: () => { starCopy.style.opacity = 1; },
-                    onComplete: () => starCopy.remove()
-                }
-            );
-        }
-    });
-}
-});
+// function addEventListenersToPieceGroup(pieceGroup, keylineGroup, starGroup) {
+//     if (pieceGroup) {
+//         pieceGroup.addEventListener('mouseover', () => {
+//             //animateKeylineAndStars(keylineGroup, starGroup);
+//         });
+
+//         pieceGroup.addEventListener('touchstart', () => {
+//             //animateKeylineAndStars(keylineGroup, starGroup);
+//         });
+
+//         pieceGroup.addEventListener('mouseout', () => {
+//             gsap.to(keylineGroup, {
+//                 opacity: 0,
+//                 duration: 1,
+//                 ease: "power1.in"
+//             });
+//         });
+//     } else {
+//         console.error("SVG group with ID 'Piece' not found.");
+//     }
+// }
+
+// function animateKeylineAndStars(keylineGroup, starGroup) {
+//     gsap.to(keylineGroup, {
+//         opacity: 1,
+//         duration: 0.3,
+//         ease: "power1.out"
+//     });
+//     starGroup.forEach((star, index) => {
+//         for (let i = 0; i < 1; i++) {
+//             const starCopy = star.cloneNode(true);
+//             star.parentNode.insertBefore(starCopy, star.nextSibling);
+
+//             gsap.fromTo(starCopy, 
+//                 { opacity: 1 },
+//                 {
+//                     motionPath: {
+//                         path: [
+//                             {x: 0, y: 0},
+//                             {x: 3, y: -3},
+//                             {x: -2, y: -6},
+//                             {x: 2, y: -9},
+//                             {x: -2, y: -12}
+//                         ],
+//                         curviness: .1,
+//                         autoRotate: false
+//                     },
+//                     opacity: 0,
+//                     duration: 1 + Math.random(),
+//                     delay: index * 0.1 + i * 0.2,
+//                     ease: "power1.out",
+//                     onStart: () => { starCopy.style.opacity = 1; },
+//                     onComplete: () => starCopy.remove()
+//                 }
+//             );
+//         }
+//     });
+// }
+// 
